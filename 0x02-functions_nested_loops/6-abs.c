cb99b560810326218b6ed0da4330e1ba5805cac1
ilef <ilef.dey@yahoo.com>
2023-02-21 15:27:20 -0800

    m

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+/**
+ * _abs - Entry point
+ * Description - Print absolute value
+ * @n: absolute value
+ * Return: int
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
