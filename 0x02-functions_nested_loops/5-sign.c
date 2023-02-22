98b3e493665f693d0f4e86781fe4313435be4f7d
ilef <ilef.dey@yahoo.com>
2023-02-21 16:07:18 -0800

    m

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,16 +1,19 @@
-/**
- * print_sign - Entry point
- * @n: Variable
- * Description: Prints the sign of a number
- * Return: 1 if positive, 0 if 0, -1 if negative
- */
+#include "main.h"
 
+/**
+ * print_sign - prints the sign of a number to stdout
+ *
+ * @n: integer to be checked
+ *
+ * Return: (1) (success), (0) (negative)
+ *
+ */
 int print_sign(int n)
 {
-	if (n > 0)
+	if (n < 0)
 	{
-		_putchar('+');
-		return (1);
+		_putchar('-');
+		return (-1);
 	}
 	else if (n == 0)
 	{
@@ -19,7 +22,7 @@ int print_sign(int n)
 	}
 	else
 	{
-		_putchar('-');
-		return (-1);
+		_putchar('+');
+		return (1);
 	}
-}
+}}
