b2d74e8dc8c3294660aafc71513fbc8c4325dcb6
ilefde <49443145+ilefde@users.noreply.github.com>
2023-03-11 09:39:16 +0800

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints its name, followed by a new line
+ * main - print number of arg argc
  * @argc: number of command line arguments
  * @argv: array containing the program command line arguments
  * Return: Always 0 (success)
  */
-int main(int argc )
+int main(int argc, char argv[] __attribute__((unused)) )
 {
 	printf("%s\n", argc-1);
 	return (0);
