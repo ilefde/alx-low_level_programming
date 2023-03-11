d032bc7b77cbe9ef85d010fc413ebcaacecb4331
ilefde <49443145+ilefde@users.noreply.github.com>
2023-03-11 09:30:51 +0800

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,8 @@
  * @argv: array containing the program command line arguments
  * Return: Always 0 (success)
  */
- int main(int argc __attribute__((unused)), char *argv[])
- {
-    printf('%s\n', argv[0]) ;
-    return(0);
- }
- 
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
