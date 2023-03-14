abd2d7a3861605a58da62c0a6bbd59f7a764e185
ilefde <49443145+ilefde@users.noreply.github.com>
2023-03-14 19:50:07 +0800

    Create 0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing or pointer 
+ */
+char *create_array(unsigned int size, char c)
+{ char *c;
+  c='a'
+    
+}
