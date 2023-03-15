b5d7b825b74d3c59c1afe3ae675a64cf15bb1bde
ilefde <49443145+ilefde@users.noreply.github.com>
2023-03-16 01:09:39 +0800

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,7 +7,7 @@
  * @c: initialization character
  * @size: the size of the memory to print
  *
- * Return: Nothing or pointer 
+ * Return: Nothing or pointer
  */
 
 char *create_array(unsigned int size, char c)
