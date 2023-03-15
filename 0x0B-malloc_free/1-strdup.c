b986724cf539db75cd3219f54c15b68d5f9a8847
ilefde <49443145+ilefde@users.noreply.github.com>
2023-03-16 01:50:05 +0800

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,3 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * **_strdup-returns a pointer to a new string
+ * @str: duplicated string
+ *
+ * Return: Nothing or pointer
+ */
+
 char *_strdup(char *str)
 {
+    char *string;
+    int x;
+    int l;
+    x=0;
+    while (str[x] != '\0')
+    {
+      x++;
+    }
+    x++;
+    strNew = malloc(sizeof(*str) * x);
+    if (strNew == NULL)
+      return (NULL);
+    string=malloc(sizeof(str))
+    while (str[y] != '\0')
+    { 
+      string[y] = str[y];
+      y++;
+    }               
+      
 }
