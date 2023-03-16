c4f16c4116d287580cdadf2cc784c5f72c37da01
ilefde <49443145+ilefde@users.noreply.github.com>
2023-03-16 16:53:10 +0800

    Create 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *malloc_checked-returns a pointer to a new string
+ * @b: duplicated string
+ *
+ * Return: Nothing or pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+  
+}
