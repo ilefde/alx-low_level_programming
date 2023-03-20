d2e37115ca481100e1650cfa42bd916327bb2f55
ilefde <49443145+ilefde@users.noreply.github.com>
2023-03-21 01:56:25 +0800

    Create 4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * *new_dog -  function that creates a new dog
+ * @name: of dog
+ * @age: of dog
+ * @owner: name of owner
+ *
+ * Return : dog_t
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+  
+}
