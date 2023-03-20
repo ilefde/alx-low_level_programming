fdee40512e032eab9f04adfc758a7c02b2d49a2d
ilefde <49443145+ilefde@users.noreply.github.com>
2023-03-21 00:54:48 +0800

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,11 +2,10 @@
 #define DOG
 
 /**
- *struct dog - new structure
- *@name: of dof
- *@age: of dog
- *@owner: name of owner
- *Description: Longer description
+ * struct dog - new structure
+ * @name: name element of dog structure
+ * @age: of dog
+ * @owner: name of owner
  */
 
 struct dog
