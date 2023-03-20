e590031fe2791c2af03f60e8fc1f64040d2d6ddf
ilefde <49443145+ilefde@users.noreply.github.com>
2023-03-21 01:02:51 +0800

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,13 +13,13 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-  if (d == NULL)
+	if (d == NULL)
 		;
 	else
 	{
-    d.name=name;
-    d.age=age;
-    d.owner=owner;
-  }
-  
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+
 }
