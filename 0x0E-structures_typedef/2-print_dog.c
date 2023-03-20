46b077be5cc365999a72b0482887e443dcc3b53c
ilefde <49443145+ilefde@users.noreply.github.com>
2023-03-20 23:20:35 +0800

    Create 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include dog.h
+
+/**
+ *void init_dog - initialize dog
+ *@*d -dog object
+ *
+ */
+
+void print_dog(struct dog *d)
+{
+  struct dog *d;
+  
+  printf( 'Name: ', d.name, '%c\n');
+  printf( 'Age: ', d.age, '%f\n');
+  printf( 'Owner: ', d.owner, '%c\n');
+         
+}
