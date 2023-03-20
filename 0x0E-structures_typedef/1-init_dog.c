cc85ecdc964fb79c38e632b69db89a213ad35cd8
ilefde <49443145+ilefde@users.noreply.github.com>
2023-03-20 23:05:14 +0800

    Create 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include dog.h
+
+/**
+ *void init_dog - initialize dog
+ *
+ * Return: dog object
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+  d.name=name;
+  d.age=age;
+  d.owner=owner;
+}
