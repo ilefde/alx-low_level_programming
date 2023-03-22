6c830df8fe383cbc5529371ca4e147d14fd8d1ac
ilefde <49443145+ilefde@users.noreply.github.com>
2023-03-22 23:34:38 +0800

    Create 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_name - prints the name of the file
+ * @name: name to be printed
+ * @f: pointer to name
+ * Return: 0
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+  
+}
