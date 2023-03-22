2ecffa69b8059d282521a5692daa4dd3c1780a59
ilefde <49443145+ilefde@users.noreply.github.com>
2023-03-23 02:45:20 +0800

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,12 +12,14 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-  size_t i = 0;
-  
-  for (i = 0; i < size ;i++)
+  int i = 0;
+  if (array && size)
   {
-    if (cmp(array[i]))
-      return (i);
+    for (i = 0; i < size ;i++)
+    {
+      if (cmp(array[i]))
+        return (i);
+    }
   }
   return(-1);
 }
