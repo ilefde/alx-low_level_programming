2ec5f0b37f83b7ff9fd122fa90180e69cba384e0
ilefde <49443145+ilefde@users.noreply.github.com>
2023-03-23 22:48:22 +0800

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -20,9 +20,9 @@
    int i;
    int j;
   
-   j = va_arg(numbers, int);
-   j = va_arg(numbers, int);
    va_start(numbers, n);
+   j = va_arg(numbers, int);
+   j = va_arg(numbers, int);
    for (i = j; i >= 0; i = va_arg(numbers, int))
    {sum = sum + i; }
    va_end(numbers);
