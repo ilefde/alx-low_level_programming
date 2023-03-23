e5f4e8cd190a3174c015f0b7da1f63811bba920e
ilefde <49443145+ilefde@users.noreply.github.com>
2023-03-23 20:12:50 +0800

    Create 0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,15 @@
+#include stdarg.h
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - Prints the result of simple operations.
+ * @n: The number of arguments supplied to the program.
+ *
+ * Return: sum of arguments.
+ */
+ 
+int sum_them_all(const unsigned int n, ...)
+{
+  
+}
