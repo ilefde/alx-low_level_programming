e586c11ea8ba7c00dbb0ff17ca9f970fc03a94b1
ilefde <49443145+ilefde@users.noreply.github.com>
2023-03-23 10:26:43 +0800

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,13 +11,13 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-  size_t i = 0;
+	size_t i = 0;
 
 	if (array && size && action)
-  {
-    for (i = 0; i < size ;i++)
-    {
-      action(array[i]);
-    }
-  }
+	{
+		for (i = 0; i < size ; i++)
+		{
+			action(array[i]);
+		}
+	}
 }
