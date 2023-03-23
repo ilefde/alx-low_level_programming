fbcab22fc58ab2f3ba82f764857770658a7ef3c8
ilefde <49443145+ilefde@users.noreply.github.com>
2023-03-24 00:45:34 +0800

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,17 +10,17 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-   va_list ap;
-   unsigned int i = 0;
-
-   va_start(ap, n); 
-   while (i < n)
+	va_list ap;
+	unsigned int i = 0;
+	
+	va_start(ap, n); 
+	while (i < n)
    {
 	   printf("%d ", va_arg(ap, int));
 	   if (separator != NULL && i < n - 1)
 	   {printf("%s", separator); }
 	   i++;
    }
-   va_end(ap);
-   putchar('\n');
+	va_end(ap);
+	printf('\n');
 }
