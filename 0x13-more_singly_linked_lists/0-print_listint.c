9ec06a0b0bc7563c58aa3e2cb6aae8a1ff259648
ilefde <49443145+ilefde@users.noreply.github.com>
2023-04-05 01:39:45 +0800

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,11 @@
 size_t print_listint(const listint_t *h)
 {
 	unsigned int nodes = 0;
+	const listint_t *current_node = h;
 
-	while (h)
+	for (; current_node; current_node = current_node->next)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", current_node->n);
 		nodes++;
 	}
 	return (nodes);
