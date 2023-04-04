4821e6c3f3a95d06aa09b2a4afe629a2c03036df
ilefde <49443145+ilefde@users.noreply.github.com>
2023-04-05 01:12:13 +0800

    Create 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,26 @@
+#include lists.h
+
+/**
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the list_t list to print
+ *
+ * Return: the number of nodes printed
+ */
+
+size_t print_listint(const listint_t *h)
+{
+  
+	size_t n = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		n++;
+	}
+
+	return (n);
+}
