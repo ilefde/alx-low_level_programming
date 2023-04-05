4c05f0de59aee4a19b31e9eaa0299d9c2c0530b9
ilefde <49443145+ilefde@users.noreply.github.com>
2023-04-05 10:34:24 +0800

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,13 +10,14 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *node;
-
-	while (*head)
+	if (*head != NULL)
 	{
-		node = (*head)->next;
-		free(*head);
-		*head = node;
+		while (*head)
+		{
+			node = (*head)->next;
+			free(*head);
+			*head = node;
+		}
 	}
-
 	*head = NULL;
 }
