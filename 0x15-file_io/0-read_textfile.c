2bab71792220e625f29a43ae5f0fd0b1ef0e6649
ilefde <49443145+ilefde@users.noreply.github.com>
2023-04-15 10:06:05 +0800

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,16 +6,19 @@
  *
  * Return: number of letter printed.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 	int fd_read, count_chars, fd_open;
 	char *buf_letters;
 
-	if (filename == NULL | letters == 0)
+	if (filename == NULL)
 		return (0);
 	fd_open = open(filename, O_RDONLY);
 	if (fd_open == -1)
 		return (0);
 	buf_letters = malloc(sizeof(char) * letters);
+	if (buf_letters == NULL)
+		return (0);
 	fd_read = read(fd_open, buf_letters, letters);
 	if (fd_read == -1)
 	{
